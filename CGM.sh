cbe2b39ef5dd89c4404aa1b4c3aa35adfbf164c7
Fanfan Wang <m18961473117@163.com>
2024-11-25 16:44:26 +0800

    Add files via upload

---
+++ CGM.sh
@@ -0,0 +1,1 @@
+python ecg.py --data_dir ./save_ec_aware_caption/test/mm_all_pred --output_dir  ./save/test --input_max_length 512 --gen_max_length 40
---
+++ ECCap.sh
@@ -0,0 +1,1 @@
+python ecg.py --data_dir ./data/ec_aware_caption  --output_dir  ./save_ec_aware_caption/test  --input_max_length 200 --gen_max_length 50 --lr 5e-5 --task_type caption
